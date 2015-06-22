7bb545fb84fa422b737101d0af3e7c5bf3b9c337
Brian <brianyu1125@gmail.com>
2015-06-21 18:04:22 -0700

    initial test

---
+++ MindBodyHwBrianYu/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+// Requires reference to WebDriver.Support.dll
+using OpenQA.Selenium.Support.UI;
+
+namespace MindBodyHwBrianYu
+{
+    [TestClass()]
+    public class Driver
+    {
+        IWebDriver driver;
+
+        [TestInitialize()]
+        public void Setup()
+        {
+            driver = new FirefoxDriver();
+            System.Console.WriteLine("created the page!");
+        }
+        [TestCleanup()]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void FirstTest()
+        {
+            Assert.AreEqual(0, 1);
+        }
+
+        [TestMethod]
+        public void Sometest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+    
+}
